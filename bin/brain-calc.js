9854185e0342050793daec7019780e3877d612a4
KonstantinZeGeR <zegelr23@gmail.com>
2024-05-20 16:05:59 +0400

    fix eslint

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import runGame from '../src/index.js';
-import { description, generateRound } from '../src/games/calc-game.js';
+import runGame from "../src/index.js";
+import { description, generateRound } from "../src/games/calc-game.js";
 
 runGame(description, generateRound);
--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,8 @@
-import nodeGlobals from "globals";
+import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 export default [
-  {
-    files: ["*.js", "*.mjs"],
-    languageOptions: {
-      globals: nodeGlobals.node,
-      ecmaVersion: 2021, // или любой другой желаемый ES стандарт
-      sourceType: "module", // или 'script' для CommonJS
-    },
-    ...pluginJs.configs.recommended,
-    rules: {
-      "no-console": "off",
-      strict: "error",
-      // добавьте любые другие правила, которые вам нужны
-    },
-  },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
