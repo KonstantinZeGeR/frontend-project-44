e62318a6c58576505f81ead96f0f5cb7f2737e2f
KonstantinZeGeR <zegelr23@gmail.com>
2024-05-20 17:35:59 +0400

    fix import all games

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,4 +1,4 @@
-import { getRandomNumber } from '../utils.js';
+import getRandomNumber from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,4 +1,4 @@
-import { getRandomNumber } from '../utils.js';
+import getRandomNumber from '../utils.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
